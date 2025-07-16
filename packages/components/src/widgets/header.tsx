5ed750442ddfe81694650e377e4dd6e0777932b8
Nikolay <kolechka.malyshev.04@gmail.com>
2025-07-16 22:28:50 +0700

    feat: add shadcn ui

--- packages/components/src/widgets/header.tsx
+++ packages/components/src/widgets/header.tsx
@@ -1,12 +1,10 @@
 import { FC } from 'react';
-import clsx from "clsx";
+import clsx from 'clsx';
 
 interface HeaderProps {
   className: string;
 }
 
-export const Header: FC<HeaderProps> = (props) => {
-  return (
-    <div className={clsx('bg-orange-400', props.className)}>123</div>
-  );
+export const Header: FC<HeaderProps> = props => {
+  return <div className={clsx('bg-orange-400', props.className)}>123</div>;
 };
