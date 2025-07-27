5f0a8e9ba13a7ababb45c7818cf5e76c39e32332
Nikolay <kolechka.malyshev.04@gmail.com>
2025-07-27 12:05:48 +0700

    main ✨ feat: fix next errors

--- apps/web/src/app/api/trpc/[trpc]/route.ts
+++ apps/web/src/app/api/trpc/[trpc]/route.ts
@@ -3,6 +3,8 @@ import {
   FetchCreateContextFnOptions,
   fetchRequestHandler,
 } from '@trpc/server/adapters/fetch';
+
+console.log('appRouter:', Object.keys(appRouter._def));
 function handler(req: Request) {
   return fetchRequestHandler({
     endpoint: '/api/trpc',
@@ -10,7 +12,9 @@ function handler(req: Request) {
     router: appRouter,
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     createContext: (_: FetchCreateContextFnOptions) => {
-      return {};
+      return {
+        session: null,
+      };
     },
   });
 }
